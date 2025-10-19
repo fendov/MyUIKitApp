1d8b6952f9039ca94dedee65d5c986775b7f03cc
Aliaksandr <Paliakou>
2025-10-19 21:36:07 +0200

    Initial Commit

---
+++ MyUIKitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyUIKitApp
+//
+//  Created by Squeze on 19/10/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
